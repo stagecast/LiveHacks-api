22fcef63b37c9e5d829ad9f11cfdcc916de1732e
Jonas Johansson <jns.johansson@gmail.com>
2018-05-09 18:44:51 +0200

    html moment example

---
+++ html-moment/README.md
@@ -0,0 +1,17 @@
+# StageCast HTML
+
+## Login
+
+1. http://staging.stagecast.se/login and login with your _staging_ credentials.
+
+## Create event
+
+1. http://staging.stagecast.se/dashboard/event/list
+2. Publish the event by clicking 'Publish' next to the event.
+
+## Create HTML moment
+
+1. Enter the newly created event and click the '+' sign.
+2. Under 'Stage Moments' choose 'Html'.
+3. Where it says 'YOUR HTML' copy/paste the contents from 'stagecast-accelerometer.html'
+4. Save and Exit.
---
+++ html-moment/accelerometer-listen.js
@@ -0,0 +1,77 @@
+const WebSocket = require('ws');
+const midi = require('midi');
+
+const eventName = 'my_unique_event';
+const input = new midi.input();
+const output = new midi.output();
+
+// console.log('Inputs:');
+// for (var i = 0; i < input.getPortCount(); i++) {
+// 	console.log('Input #'+i+': ' + input.getPortName(i));
+// }
+// console.log();
+
+// console.log('Outputs:');
+// for (var i = 0; i < output.getPortCount(); i++) {
+// 	console.log('Output #'+i+': ' + output.getPortName(i));
+// }
+// console.log();
+
+output.openPort(0);
+
+const ws = new WebSocket(`ws://stagecast.se/api/events/${eventName}/ws?x-user-listener=1`);
+
+const handleMessage = m => {
+	if (!m.msg) {
+		return;
+	}
+
+	// console.log('m',m);
+
+	const rawx = m.msg.rawacc[0];
+	const rawy = m.msg.rawacc[1];
+	const rawr = m.msg.rawacc[2];
+
+	let x = rawx.map(0, 360, 0, 127);
+	let y = rawy.map(-90, 90, 0, 127);
+	let r = rawr.map(-180, 180, 0, 127);
+
+	x = Math.round(x);
+	y = Math.round(y);
+	r = Math.round(r);
+
+	// console.log(`_x: ${rawx}, _y: ${rawy}, _r: ${rawr}`);
+	// console.log(`x: ${x}, y: ${y}, r: ${r}`);
+
+	output.sendMessage([0xB0, 41, x]);
+};
+
+// ws.on('open', () => {
+// 	setInterval(() => {
+// 		handleMessage({msg: {rawacc: [0,-90,-180]}})
+// 		setTimeout(() => {
+// 			handleMessage({msg: {rawacc: [180,0,0]}})
+// 			setTimeout(() => {
+// 				handleMessage({msg: {rawacc: [360,90,180]}})
+// 			},500);
+// 		},500);
+// 	},1500);
+// });
+
+ws.on('message', data => {
+	try {
+		data = JSON.parse(data);
+		handleMessage(data);
+	} catch (e) {
+		console.error(e);
+	}
+});
+
+Number.prototype.map = function (in_min, in_max, out_min, out_max) {
+	return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+};
+
+process.on('SIGINT', () => {
+	output.closePort();
+	process.exit(2);
+});
