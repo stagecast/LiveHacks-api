651074c21b5ca2d8491067b8b7b8c3ee3854f6e5
Jonas Johansson <jns.johansson@gmail.com>
2019-08-31 16:52:54 +0200

    Update html-receive.js

--- html-moments/html-moment-example/html-receive.js
+++ html-moments/html-moment-example/html-receive.js
@@ -4,38 +4,14 @@ const ws = new WebSocket(`wss://stagecast.se/api/events/${channelName}/ws`);
 const authString = `{ "email" : "jonasjohansson@stagecast.se", "password" : "Ciao1234" }`;
 
 const handleMessage = m => {
-	if (!m.msg) {
-		return;
-	}
-
-	// console.log('m', m);
-
-	const rawx = m.msg.rawacc[0];
-	const rawy = m.msg.rawacc[1];
-	const rawr = m.msg.rawacc[2];
-
-	let x = rawx.map(0, 360, 0, 127);
-	let y = rawy.map(-90, 90, 0, 127);
-	let r = rawr.map(-180, 180, 0, 127);
-
-	x = Math.round(x);
-	y = Math.round(y);
-	r = Math.round(r);
-
-	console.log(`_x: ${rawx}, _y: ${rawy}, _r: ${rawr}`);
-	console.log(`x: ${x}, y: ${y}, r: ${r}`);
-
-	// output.sendMessage([0xb0, 41, x]);
-	// output.sendMessage([0xb0, 42, y]);
-	// output.sendMessage([0xb0, 43, r]);
+	console.log('m', m);
 };
 
 ws.on('open', () => {
-	// if (auth !== '') ws.send(`${auth}`);
+	if (authString !== '') ws.send(`${authString}`);
 });
 
 ws.on('message', data => {
-	console.log(data.toString());
 	if (data.toString() === 'ok') {
 		return;
 	}
