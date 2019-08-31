18c98f7e1f6710aa9f844bd28fc09bcb512c746b
Jonas Johansson <jns.johansson@gmail.com>
2019-08-31 16:56:45 +0200

    updat examples, minimal now only sends on load

--- html-moments/html-moment-example/html-receive.js
+++ html-moments/html-moment-example/html-receive.js
@@ -23,10 +23,6 @@ ws.on('message', data => {
 	}
 });
 
-Number.prototype.map = function(in_min, in_max, out_min, out_max) {
-	return ((this - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-};
-
 process.on('SIGINT', () => {
 	process.exit(2);
 });
